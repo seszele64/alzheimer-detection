ba19f4bcde7c7b5d20b39c4b57ca189cd6cdfab7
seszele64 <76787567+seszele64@users.noreply.github.com>
2024-08-09 10:34:45 +0200

    added docstrings

--- model/alzheimers_model.py
+++ model/alzheimers_model.py
@@ -1,3 +1,7 @@
+"""
+This code defines a custom neural network called AlzheimerNet. It uses a pre-trained EfficientNet-B0 model as the base and modifies the classifier layer to output the desired number of classes (4 in this case, for different stages of Alzheimer's). The forward method defines how input data flows through the network
+"""
+
 import torch.nn as nn
 from torchvision import models
 
